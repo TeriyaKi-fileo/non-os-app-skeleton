22083e547bec18c61e6a30de192ad3f4dc9d98c0
TeriyaKi-fileo <138987768+TeriyaKi-fileo@users.noreply.github.com>
2026-04-11 14:32:35 +0900

    戻す

--- src/graphics.c
+++ src/graphics.c
@@ -70,7 +70,7 @@ void _drawPixel(int x, int y, unsigned char color_id) {
     unsigned int index;
     // 画面外への書き込みを防止
     if (x >= 0 && x < V_WIDTH && y >= 0 && y < V_HEIGHT) {
-        index = (y * V_WIDTH) + (x | 0x01) - 1;
+        index = (y * V_WIDTH) + (x | 0x01);
         c = vram[index];
         if (x & 0x01) {
             col = col & 0x0F;
